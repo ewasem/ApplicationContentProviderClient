6bdd19e09330fb9e496a89ea14a8a8220fce8b50
ewasem <eduardowasem@outlook.com>
2021-10-05 11:32:04 -0300

    Cliente do content provider criado

---
+++ app/src/main/java/com/ewapps/applicationcontentproviderclient/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ewapps.applicationcontentproviderclient
+
+import android.database.Cursor
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var notesRecycler: RecyclerView
+    lateinit var notesRefreshButton: FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notesRefreshButton = findViewById(R.id.btn_refresh)
+        notesRecycler = findViewById(R.id.rv_client_list)
+
+        getContentProvider()
+
+        notesRefreshButton.setOnClickListener {
+            getContentProvider()
+        }
+    }
+
+    private fun getContentProvider() {
+        try{
+            val url="content://com.ewapps.applicationcontentprovider.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor?= contentResolver.query(data, null, null, null,"title")
+            notesRecycler.apply{
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        }catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
+}
